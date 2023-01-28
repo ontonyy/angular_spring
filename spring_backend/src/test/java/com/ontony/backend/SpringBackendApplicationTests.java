ea478d7c89386a6f4cb8e59810acc498c6aa7e88
Anton Gavrilin <anton.gavrilin@betpawa.com>
2023-01-28 14:11:54 +0200

    Create inital wireframe of spring backend application

---
+++ spring_backend/src/test/java/com/ontony/backend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontony.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
